f778074df2b0bdc953a2f172a5790333a29054d4
Abhijit-Singh <abhijits1280@gmail.com>
2026-03-13 23:03:48 +0530

    Update config.mjs

--- bookmanagement/config.mjs
+++ bookmanagement/config.mjs
@@ -3,15 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  port: process.env.PORT || 3000,
-  mongoURI: process.env.MongoDB,
-  JWTSecret: process.env.JWT_SECRET,
-  aws: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
-    bucketName: process.env.AWS_S3_BUCKET_NAME
-  }
+    port: process.env.PORT || 8080,
+    mongoURI: process.env.MongoDB,
+    // JWTSecret: process.env.JWT_SECRET,
+    JWTsecretToken: process.env.JWT_secretToken,
+    accessKey: process.env.accessKey,
+    secretAccessKey: process.env.secretAccessKey,
+    region: process.env.region
 };
 
 export default config;
